75dac10ede09de3cf2418aeb019fd98c6755949a
AlvaroMarcha <alvarochavas1994@gmail.com>
2021-11-12 14:29:15 +0100

    Version1.0 - angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InformesComponent } from './informes/informes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ClientesComponent,
+    ProductosComponent,
+    ContactoComponent,
+    InformesComponent
   ],
   imports: [
     BrowserModule
